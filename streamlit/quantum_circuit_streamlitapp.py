c7cedbc4aadeb809f02526eb34cc05622c6eee7e
Husain Wafaie <husainwafaie@gmail.com>
2023-08-05 19:22:39 -0700

    Saving and Rendering functions Implemented

--- streamlit/quantum_circuit_streamlitapp.py
+++ streamlit/quantum_circuit_streamlitapp.py
@@ -1,10 +1,35 @@
 import streamlit as st
+import numpy as np
 from qiskit import QuantumCircuit, execute, Aer
 from qiskit.visualization import circuit_drawer, plot_state_city
 import matplotlib.pyplot as plt
-r=0
-arr = []
-def update_circuit_visualization(circuit, gate_operations):
+from streamlit_ws_localstorage import injectWebsocketCode, getOrCreateUID
+
+conn = injectWebsocketCode(hostPort='linode.liquidco.in', uid=getOrCreateUID())
+
+def update(circuit, circuit_visualization, **n):
+    opr = st.session_state.gate_operations.copy()
+
+    while len(opr) > st.session_state._qubit:
+            opr.pop()
+            
+    for qubit, gate_ops in enumerate(opr):
+        for gate in gate_ops:
+            if gate == "Hadamard":
+                circuit.h(qubit)
+            elif gate == "Pauli-X":
+                circuit.x(qubit)
+            elif gate == "Pauli-Y":
+                circuit.y(qubit)
+            elif gate == "Pauli-Z":
+                circuit.z(qubit)
+            elif gate == "CNOT":
+                control = (qubit + 1) % st.session_state._qubit
+                circuit.cx(qubit, control)
+
+    circuit_visualization.image(update_circuit_visualization(circuit), use_column_width=True)
+
+def update_circuit_visualization(circuit):
     circuit_drawer_file = "circuit_drawer.png"
 
     circuit_drawer(circuit, output='mpl', filename=circuit_drawer_file)
@@ -14,22 +39,33 @@ def update_circuit_visualization(circuit, gate_operations):
 
 
 def main():
-    global r
-    global arr
+    global conn
+    if "state_number" not in st.session_state:
+        st.session_state.state_number = 0
     st.title("Quantum Composer")
 
     # Sidebar options
     num_qubits = st.sidebar.number_input("Number of Qubits", min_value=1, max_value=5, value=2, step=1)
 
+
+    gate_operations = [[] for _ in range(num_qubits)]
+    # Initialize session-states
+    if "_qubit" not in st.session_state:
+        st.session_state._qubit = num_qubits
+
+    if st.session_state._qubit != num_qubits:
+        st.session_state._qubit = num_qubits;
+        if len(st.session_state.gate_operations) < st.session_state._qubit:
+            # st.session_state.gate_operations.append([])
+            st.session_state.gate_operations.append([])
+
+    if "gate_operations" not in st.session_state:
+        st.session_state.gate_operations = gate_operations
+
+        
     # Initialize a Quantum Circuit
-    circuit = QuantumCircuit(num_qubits)
-
-    if r == 0:
-        r += 1
-        arr = [[] for _ in range(num_qubits)]
-        gate_operations = [[] for _ in range(num_qubits)]
-        print(r)
-
+    circuit = QuantumCircuit(st.session_state._qubit)
+    
     # Quantum Gates
     gate_options = ["Hadamard", "Pauli-X", "Pauli-Y", "Pauli-Z", "CNOT"]
 
@@ -40,8 +76,25 @@ def main():
     # Add Gates to the Circuit
     st.sidebar.subheader("Add Gates")
 
-    element= True
+    # element= True
 
+    if st.sidebar.button("Save", key="s"):
+
+        r = st.session_state.gate_operations.copy()
+        # for i in range()
+        print(r)
+        for i in r:
+            i.append(1)
+        print(r)
+        conn.setLocalStorageVal(key=st.session_state.state_number, val=r)
+        for i in r:
+            i.remove(1)
+        del r
+
+        
+
+
+        st.session_state.state_number += 1
     for qubit in range(num_qubits):
         gate_label = f"Gate - Qubit {qubit}"
         selected_gate = st.sidebar.selectbox(gate_label, gate_options, key=f"gate-{qubit}")
@@ -50,40 +103,34 @@ def main():
 
         if st.sidebar.button(f"Apply Gate {qubit}", key=f"apply-{qubit}"):
             
-            print(arr)
-            gate_operations[selected_qubit].append(selected_gate)
-            arr[selected_qubit].append(selected_gate)
-            print(arr)
+            st.session_state.gate_operations[selected_qubit].append(selected_gate)
 
-            # Reset the circuit to remove the previously applied gates
-            circuit = QuantumCircuit(num_qubits)
 
-            while element:
 
-                # Update the circuit with the selected gates
-                for qubit, gate_ops in enumerate(gate_operations):
-                    for gate in gate_ops:
-                        if gate == "Hadamard":
-                            circuit.h(qubit)
-                        elif gate == "Pauli-X":
-                            circuit.x(qubit)
-                        elif gate == "Pauli-Y":
-                            circuit.y(qubit)
-                        elif gate == "Pauli-Z":
-                            circuit.z(qubit)
-                        elif gate == "CNOT":
-                            control = (qubit + 1) % num_qubits
-                            circuit.cx(qubit, control)
+            
+            
+            
+            update(circuit, circuit_visualization)
                     
-                if st.sidebar.subheader("Finish circuit"):
-                    element= False
-
-                # Update the Circuit Visualization
-                image = update_circuit_visualization(circuit, gate_operations)
-                circuit_visualization.image(image, use_column_width=True)
-                
 
 
+    if st.session_state.state_number:
+        for i in range(st.session_state.state_number):
+            if st.sidebar.button(f"state_${i + 1}", key=i):
+                vt= conn.getLocalStorageVal(key=i)
+                parsed_arr_1 = vt.split(",")
+                r = [[]]
+
+                for t in parsed_arr_1:
+                    if t.isdigit():
+                        r.append([])
+                    else:
+                        r[-1].append(t)
+            
+
+
+                st.session_state.gate_operations = r
+                update(circuit, circuit_visualization)
     # Execute the Circuit
     st.sidebar.subheader("Execute Circuit")
     backend_options = ["qasm_simulator", "statevector_simulator", "unitary_simulator"]
@@ -93,8 +140,11 @@ def main():
         # Reset the circuit to remove the previously applied gates
         circuit = QuantumCircuit(num_qubits)
 
+
+        
+
         # Update the circuit with the selected gates
-        for qubit, gate_ops in enumerate(gate_operations):
+        for qubit, gate_ops in enumerate(st.session_state.gate_operations):
             for gate in gate_ops:
                 if gate == "Hadamard":
                     circuit.h(qubit)
